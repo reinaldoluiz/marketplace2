974c7c0273be96f009897b76431c10a68141bb98
reinaldo luiz <reinaldolluiz@gmail.com>
2020-09-16 20:42:06 -0300

    User search a product or a category

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def search 
+    @products = Product.where('name like ? OR category like ?', "%#{params[:q]}%", "%#{params[:q]}%")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  
+  get 'search', to:'home#search'
   resources :products, only:[:index, :show, :new, :create] do
     resources :comments, module: :products
     resources :orders,  only:[:create, :show]
